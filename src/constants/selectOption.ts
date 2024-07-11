1f7fe6bfdcf94d263113974a71387f557bd8e81a
Kim Kayoung <kykim.dev@gmail.com>
2024-07-12 08:41:00 +0900

    Seperate select options as constants

---
+++ src/constants/selectOption.ts
@@ -0,0 +1,11 @@
+export const BARCHARTOPTION = [
+  { label: 'Highest', value: 'highest' },
+  { label: 'Lowest', value: 'lowest' },
+]
+
+export const RADARCHARTOPTION = [
+  { value: 'pikachu', label: '피카츄' },
+  { value: 'squirtle', label: '꼬부기' },
+  { value: 'charmeleon', label: '리자드' },
+  { value: 'bulbasaur', label: '이상해씨' },
+]
